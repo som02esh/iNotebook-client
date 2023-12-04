231abfafdb8791af4f239b0b9183f327481517ae
som02esh <106095040+som02esh@users.noreply.github.com>
2023-12-04 10:54:31 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,9 +11,8 @@ function Navbar() {
   }
   return (
     <nav className="navbar navbar-expand-lg  navbar-light bg-danger">
-    <div className="container-fluid">
       <Link className="navbar-brand" to="/">iNotebook</Link>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button> 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -28,7 +27,6 @@ function Navbar() {
           </form>
         </ul>
       </div>
-    </div>
   </nav> 
   )
 }
